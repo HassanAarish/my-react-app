955a853943bfe1044d63ea89397da87f3517dca8
Hassan Aarish <aarishhassan98@gmail.com>
2024-10-21 13:45:51 +0500

    import export and require module differences.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Heading from "./components/Heading";
 import List from "./components/List";
+import { Add, Sub, Multi, Divide } from "../src/components/Calculator";
 
 function App() {
   return (
     <div className="App">
       <Heading />
       <List />
+      <ul>
+        <li>{Add(1, 2)}</li>
+        <li>{Multi(2, 3)}</li>
+        <li>{Sub(7, 2)}</li>
+        <li>{Divide(5, 2)}</li>
+      </ul>
     </div>
   );
 }
