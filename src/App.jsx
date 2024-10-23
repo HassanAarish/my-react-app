7adeb3c182090418ea14e2f9165f829123332802
Hassan Aarish <aarishhassan98@gmail.com>
2024-10-23 13:09:26 +0500

    Keeper-app-part2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Entry from "./components/Entry";
-import emojipedia from "./data/emojipedia";
-
-const newEmojipedia = emojipedia.map(function (emojiEntry) {
-  return emojiEntry.meaning.substring(0, 100);
-});
-console.log("🚀 ~ Emoji Data:", emojipedia);
-console.log("🚀 ~ Emoji result:", newEmojipedia);
+import Note from "./components/Note";
+import notes from "./data/notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <div>
-        <h1>
-          <span>emojipedia</span>
-        </h1>
-
-        <dl className="dictionary">
-          {emojipedia.map((emojipedia) => (
-            <Entry
-              key={emojipedia.id}
-              id={emojipedia.id}
-              emoji={emojipedia.emoji}
-              name={emojipedia.name}
-              description={emojipedia.meaning}
-            />
-          ))}
-        </dl>
-      </div>
+      {notes.map((notes) => (
+        <Note
+          key={notes.key}
+          id={notes.key}
+          title={notes.title}
+          content={notes.content}
+        />
+      ))}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Avatar from "./Avatar";
 
 function Note(Props) {
   return (
     <div className="note">
-      <h1>{Props.title}</h1>
+      <h1>
+        {Props.id} : {Props.title}
+      </h1>
       <p>{Props.content}</p>
-      <Avatar img="https://picsum.photos/200" />
     </div>
   );
 }
